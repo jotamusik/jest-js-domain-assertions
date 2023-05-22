ded23aa401860113ad9a3862c83c57aed8179fe2
Jorge Aguiar Martin <jorge@leanmind.es>
2023-05-22 23:17:01 +0100

    removing semicolons

--- src/person.js
+++ src/person.js
@@ -1,19 +1,18 @@
-
 export default class Person {
   constructor(name, age) {
-    this._name = name;
-    this._age = age;
+    this._name = name
+    this._age = age
   }
 
   get name() {
-    return this._name;
+    return this._name
   }
 
   get age() {
-    return this._age;
+    return this._age
   }
 
   toString() {
-    return `Person(name=${this.name}, age=${this.age})`;
+    return `Person(name=${this.name}, age=${this.age})`
   }
 }
